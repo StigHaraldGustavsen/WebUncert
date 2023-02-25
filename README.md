965cc8e1d732f4e3217eb6ccaca4f8ec73c35ad3
StigHaraldGustavsen <stighg@gmail.com>
2023-02-25 23:13:33 +0100

    inital commit with a local working flask app

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# WebUncert
-A flask backend web service for calculatating random uncertanity
+A web service that receives a JSON paylod of samples and returns a JSON payload the random uncertanity of the samples
+
+input HTTP body:
+```JSON
+{"samples": [1000.1, 999.9, 1000.0, 998.9, 1000.0, 1000.8, 1002.7]}
+```
+returns:
+```JSON
+{
+    "confidence_intervall": 0.95,
+    "dof": 6,
+    "mean": 1000.342857142857,
+    "n": 7,
+    "repetability": 0.0037986975893979894,
+    "samples": [
+        1000.1,
+        999.9,
+        1000.0,
+        998.9,
+        1000.0,
+        1000.8,
+        1002.7
+    ],
+    "standard_deviaton": 0.0014048437830613866,
+    "uncertanity_fraction": 0.0012999715833845495,
+    "uncertanity_in_unit": 1.300417287927424,
+    "uncertanity_prcent": 0.12999715833845493,
+    "uncertanity_from_repetatbilty": true,
+}
+```
+
+run locally:
+```Bash
+poetry install
+poetry shell
+flask --app webuncert/app run
+```
+
+valid JSON raw input:
+```JSON
+ {
+    "samples": [1000.1, 999.9, 1000.0, 998.9, 1000.0, 1000.8, 1002.7],
+    "stdev": "bool",
+    "confidence_interval": "0.0-1.0 or 1-100%",
+    "coverage_factor": "float", 
+ }
+```
+Note:
+if both confidence_interval and coverage factor is sent in the body, then coverage factor base uncert will be returned.
---
+++ webuncert/app.py
@@ -0,0 +1,34 @@
+from flask import Flask, jsonify, request
+
+from meter_proving.meter_proving import calculate_uncertanity
+
+app = Flask(__name__)
+
+@app.route("/")
+def GetUncert():
+    from_repetability = True
+    print(request.json)
+    x = request.json['samples']
+    if 'stdev' in request.json:
+        from_repetability = not request.json['stdev']
+
+    if 'confidence_interval' in request.json:
+        confidence_interval = request.json['confidence_interval']
+        if confidence_interval> 1.0: #guess the input is in prcent and not in a fraction
+            confidence_interval = confidence_interval/100
+    
+    if 'coverage_factor' in request.json:
+        coverage_factor = request.json['coverage_factor']
+    
+    print(request.json)
+    if 'confidence_interval' in request.json and not 'coverage_factor' in request.json:
+        uncert = calculate_uncertanity(x,repetability=from_repetability,confidence_intervall=confidence_interval)
+    elif 'coverage_factor' in request.json:
+        uncert = calculate_uncertanity(x,repetability=from_repetability,coverage_factor=coverage_factor)
+    else: 
+        uncert = calculate_uncertanity(x,repetability=from_repetability)
+    
+    uncert['samples'] = x #addint samples to the dict
+    uncert['uncertanity_from_repetatbilty'] = from_repetability
+    
+    return jsonify(uncert)
